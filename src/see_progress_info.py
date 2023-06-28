db13ad0bd614fb3b517b9a4db7be4e3551838c7f
Achkan Salehi <ashsalehi4133@gmail.com>
2023-06-29 00:21:05 +0200

    minor hyperparam stuff

--- src/see_progress_info.py
+++ src/see_progress_info.py
@@ -10,8 +10,10 @@ if __name__=="__main__":
     with open(fn,"r") as fl:
         progress_dict=json.load(fl)
 
-
-    plt.plot(progress_dict["train_loss"],"r")
-    plt.plot(progress_dict["val_loss"],"b")
+    print("train_loss:\n",progress_dict["train_loss"])
+    print("val_loss:\n",progress_dict["val_loss"])
+    plt.plot(progress_dict["train_loss"],"r",label="train")
+    plt.plot(progress_dict["val_loss"],"b",label="validation")
     plt.title(f"epoch with best val loss={progress_dict['epoch_with_best_val_loss']}")
+    plt.legend(fontsize=16)
     plt.show()
